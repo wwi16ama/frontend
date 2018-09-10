7647f16e9419e608c20e2a8a408dbe0e449fcfa1
Thomas <thomasbirke@gmx.de>
2018-09-10 15:02:49 +0200

    01.001F: Add memberview route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { NgModule } from '@angular/core';
 import { MemberListComponent } from './member-list/member-list.component';
+import { MemberViewComponent } from './member-view/member-view.component';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/memberlist', pathMatch: 'full' },
-  { path: 'memberlist', component: MemberListComponent }
+  { path: 'memberlist', component: MemberListComponent },
+  { path: 'member/:id', component: MemberViewComponent }
 ];
 
 @NgModule({
