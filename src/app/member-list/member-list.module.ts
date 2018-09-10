331e0eac0bc749395c83080f13bbeeeb678b8b69
Thomas <thomasbirke@gmx.de>
2018-09-10 09:10:33 +0200

    01.001F: Create member list module and component

---
+++ src/app/member-list/member-list.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MemberListComponent } from './member-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    MemberListComponent
+  ],
+  exports: [
+    MemberListComponent
+  ]
+})
+export class MemberListModule { }
