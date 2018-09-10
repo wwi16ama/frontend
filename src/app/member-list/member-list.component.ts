170e9d81d72e971e9b58b12f0b9185b78a78d7ac
Thomas <thomasbirke@gmx.de>
2018-09-10 10:23:40 +0200

    01.001F: Add member list table

--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { MatSort, MatTableDataSource } from '@angular/material';
+import { ListMember } from './../models/list-member.model';
+import { MemberListService } from './../services/memberlist.service';
 
 @Component({
   selector: 'app-member-list',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberListComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['id', 'firstName', 'lastName'];
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(public memberListService: MemberListService) { }
 
   ngOnInit() {
+    this.memberListService.getMemberListData().subscribe(
+      (data: ListMember[]) => {
+        this.dataSource = new MatTableDataSource(data);
+        this.dataSource.sort = this.sort;
+      }
+    );
   }
 
 }
