d72fe5e64b544ea7aa41c9fba701c3c388e2b107
Thomas <thomasbirke@gmx.de>
2018-09-20 09:35:39 +0200

    Add route in test

--- src/app/member-list/member-list.component.spec.ts
+++ src/app/member-list/member-list.component.spec.ts
@@ -1,12 +1,18 @@
+import { of } from 'rxjs';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MemberListComponent } from './member-list.component';
+import { MemberViewModule } from './../member-view/member-view.module';
 import { MatTableModule, MatSortModule } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
 
-import { Router } from '@angular/router';
-import { AppRoutingModule } from './../app-routing.module';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTableDataSource } from '@angular/material';
+import { MemberListService } from './../services/memberlist.service';
+import { ListMember } from './../models/list-member.model';
 
 describe('MemberListComponent', () => {
   let component: MemberListComponent;
@@ -15,17 +21,21 @@ describe('MemberListComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        RouterTestingModule.withRoutes([
+          { path: 'member/:id', component: MemberListComponent },
+      ]),
         MatTableModule,
         MatSortModule,
         HttpClientModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        MemberViewModule
       ],
-      declarations: [ MemberListComponent ],
+      declarations: [MemberListComponent],
       providers: [
-        { provide: Router, useClass: AppRoutingModule },
+        MemberListService
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -37,4 +47,51 @@ describe('MemberListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  describe('Variables', () => {
+    it('should define variables', () => {
+      expect(component.displayedColumns).toBeDefined();
+    });
+
+    it('should define the default values', () => {
+      const testDisplayedColumnsArray: string[] = ['id', 'firstName', 'lastName'];
+      expect(component.displayedColumns).toEqual(testDisplayedColumnsArray);
+    });
+  });
+
+  describe('ngOnInit', () => {
+    it('should define the method', () => {
+      expect(component.ngOnInit).toBeDefined();
+    });
+
+    it('should call getMemberListData from memberListService', () => {
+      const dumyGetMemberListData = [new ListMember(0, 'Peter', 'Zwegat'), new ListMember(1, 'Max', 'Mustermann')];
+      spyOn(component.memberListService, 'getMemberListData').and.returnValue(of(dumyGetMemberListData));
+      component.ngOnInit();
+      expect(component.memberListService.getMemberListData).toHaveBeenCalledTimes(1);
+    });
+
+    it('should set dataSource', () => {
+      const dumyGetMemberListData = [new ListMember(0, 'Peter', 'Zwegat'), new ListMember(1, 'Max', 'Mustermann')];
+      spyOn(component.memberListService, 'getMemberListData').and.returnValue(of(dumyGetMemberListData));
+      component.ngOnInit();
+      // expect(component.dataSource).(new MatTableDataSource(dumyGetMemberListData));
+    });
+  });
+
+  describe('navigateTo', () => {
+    it('should define the method', () => {
+      expect(component.navigateTo).toBeDefined();
+    });
+
+    it('should call router navigate with  parameter', () => {
+      const testId = 0;
+      const testParams = ['/member', testId];
+      spyOn(component.router, 'navigate');
+      component.navigateTo(testId);
+      expect(component.router.navigate).toHaveBeenCalledWith(testParams);
+    });
+  });
+
 });
