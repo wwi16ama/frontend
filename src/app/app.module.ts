711d1a427add32d864cb1824a2702ae0171a5a4d
Thomas <thomasbirke@gmx.de>
2018-09-24 18:28:30 +0200

    01.003F: Clean app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MAT_DATE_LOCALE } from '@angular/material';
+import { NgModule } from '@angular/core';
+import localeDe from '@angular/common/locales/de';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 import { AppComponent } from './app.component';
 import { AddUserFormComponent } from './add-user-form/add-user-form.component';
-import { NumberOnlyDirective } from './number.directive';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MemberListModule } from './member-list/member-list.module';
@@ -15,16 +19,17 @@ import { CreditListModule } from './credit-list/credit-list.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AddUserFormComponent,
-    NumberOnlyDirective
+    AppComponent
   ],
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MemberListModule,
     MemberViewModule,
     PlaneListModule,
     CreditListModule
+    MemberViewModule
   ],
   providers: [
     { provide: MAT_DATE_LOCALE, useValue: 'ger' }
@@ -34,6 +39,8 @@ import { CreditListModule } from './credit-list/credit-list.module';
     EditMemberDialogComponent,
     EditPlaneDialogComponent
   ]
+  providers: [{ provide: localeDe, useValue: 'de' }],
+  bootstrap: [AppComponent]
 })
-export class AppModule { 
- }
+export class AppModule {
+}
