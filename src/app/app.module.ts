513430659985ef33e558e70288921468ae4fb03d
Thomas <thomasbirke@gmx.de>
2018-09-24 22:48:58 +0200

    01.004F: Add editMemberViewComponent as entrycomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MAT_DATE_LOCALE } from '@angular/material';
 import { AppComponent } from './app.component';
 
-import { AppRoutingModule } from './app-routing.module'
+import { AppRoutingModule } from './app-routing.module';
 import { MemberListModule } from './member-list/member-list.module';
 import { MemberViewModule } from './member-view/member-view.module';
+import { EditMemberDialogComponent } from './member-view/edit-member-dialog/edit-member-dialog.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,12 @@ import { MemberViewModule } from './member-view/member-view.module';
     MemberListModule,
     MemberViewModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'ger' }
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    EditMemberDialogComponent
+  ]
 })
 export class AppModule { }
