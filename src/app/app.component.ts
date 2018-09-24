0295820b4e55f4facf6126809a808c20de255503
Thomas <thomasbirke@gmx.de>
2018-09-24 18:28:45 +0200

    01.003F: Clean app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,8 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material';
-import { AddUserFormComponent } from './add-user-form/add-user-form.component';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'wwi16ama';
-
-  constructor(public dialog: MatDialog) {}
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(AddUserFormComponent, {
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
-  }
-}
+export class AppComponent { }
