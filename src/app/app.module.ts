1039eac302bdafbd95856ae5c517b8caafc81452
Thomas <thomasbirke@gmx.de>
2018-09-24 18:28:30 +0200

    01.003F: Clean app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MAT_DATE_LOCALE } from '@angular/material';
+import { NgModule } from '@angular/core';
+import localeDe from '@angular/common/locales/de';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 import { AppComponent } from './app.component';
 import { AddUserFormComponent } from './add-user-form/add-user-form.component';
-import { NumberOnlyDirective } from './number.directive';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MemberListModule } from './member-list/member-list.module';
@@ -19,11 +23,11 @@ import { DeletePlaneDialogComponent } from './plane-list/delete-plane-dialog/del
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AddUserFormComponent,
-    NumberOnlyDirective
+    AppComponent
   ],
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MemberListModule,
     MemberViewModule,
@@ -42,6 +46,8 @@ import { DeletePlaneDialogComponent } from './plane-list/delete-plane-dialog/del
     EditPlaneDialogComponent,
     DeletePlaneDialogComponent
   ]
+  providers: [{ provide: localeDe, useValue: 'de' }],
+  bootstrap: [AppComponent]
 })
-export class AppModule { 
- }
+export class AppModule {
+}
