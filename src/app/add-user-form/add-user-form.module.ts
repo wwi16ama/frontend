af0be7d6e599b60c42bc72c6dd96ae2f8cd780c1
Thomas <thomasbirke@gmx.de>
2018-09-24 18:28:18 +0200

    01.003F: Add module for AddUserComponent

---
+++ src/app/add-user-form/add-user-form.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AddUserFormComponent } from './add-user-form.component';
+import { NumberOnlyDirective } from './../directives/number.directive';
+
+import {
+  MatButtonModule, MatCheckboxModule, MatFormFieldModule,
+  MatInputModule, MatSelectModule, MatStepperModule,
+  MatCardModule, MatTooltipModule, MatDatepickerModule,
+  MatNativeDateModule, MatDialogModule
+} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatStepperModule,
+    MatCardModule,
+    MatTooltipModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    AddUserFormComponent,
+    NumberOnlyDirective
+  ],
+  exports: [
+    AddUserFormComponent
+  ]
+})
+export class AddUserFormModule { }
