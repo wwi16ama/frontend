59145143e6e630293bc9788aa2ed530d47984aa9
Thomas <thomasbirke@gmx.de>
2018-09-28 20:47:10 +0200

    01.004F: Init member-update service test

--- src/app/services/member-update.service.spec.ts
+++ src/app/services/member-update.service.spec.ts
@@ -1,12 +1,44 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 
 import { MemberUpdateService } from './member-update.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-describe('MemberUpdateService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+import { Member, Gender, Address, Status, Authorization, AuthorizationEnum, Office, OfficeEnum } from './../models/member.model';
+
+
+describe('MemberService', () => {
+
+  let injector: TestBed;
+  let service: MemberUpdateService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        MemberUpdateService
+      ]
+    });
+    injector = getTestBed();
+    service = injector.get(MemberUpdateService);
+    httpMock = injector.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
 
   it('should be created', () => {
-    const service: MemberUpdateService = TestBed.get(MemberUpdateService);
     expect(service).toBeTruthy();
   });
+
+  describe('updateMemberData', () => {
+    it('should define the method', () => {
+      expect(service.updateMemberData).toBeDefined();
+    });
+  });
+
 });
+
