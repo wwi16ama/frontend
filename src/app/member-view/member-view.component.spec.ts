fe3aa523b5fab0215215221f57cd1a65083c0e3d
Thomas <thomasbirke@gmx.de>
2018-09-28 20:46:31 +0200

    01.004F: Update member-view test

--- src/app/member-view/member-view.component.spec.ts
+++ src/app/member-view/member-view.component.spec.ts
@@ -3,11 +3,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MemberViewComponent } from './member-view.component';
 import { MemberService } from './../services/member.service';
-import { MatDividerModule, MatListModule, MatCheckboxModule } from '@angular/material';
+import { MemberUpdateService } from './../services/member-update.service';
+import {
+  MatDividerModule, MatListModule, MatCheckboxModule,
+  MatButtonModule, MatSelectModule, MatInputModule, MatSnackBarModule
+} from '@angular/material';
+import { MatDialog, MatSnackBar } from '@angular/material';
+
 import { HttpClientModule } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { Member, Gender, Address, Authorization, AuthorizationEnum, Status, Office, OfficeEnum } from './../models/member.model';
-
 describe('MemberViewComponent', () => {
   let component: MemberViewComponent;
   let fixture: ComponentFixture<MemberViewComponent>;
@@ -18,6 +23,10 @@ describe('MemberViewComponent', () => {
         MatDividerModule,
         MatListModule,
         MatCheckboxModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatInputModule,
+        MatSnackBarModule,
         HttpClientModule
       ],
       declarations: [MemberViewComponent],
@@ -28,7 +37,10 @@ describe('MemberViewComponent', () => {
             params: of([{ id: '0' }]),
           },
         },
-        MemberService
+        { provide: MatDialog, useValue: {} },
+        { provide: MatSnackBar, useValue: {} },
+        MemberService,
+        MemberUpdateService
       ]
     })
       .compileComponents();
