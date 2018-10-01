fa88f929749dfe9e13964fbe0f6ca20402cf8532
Unknown <pia.rittler@gmx.de>
2018-10-01 14:53:32 +0200

    03.001F import plane list module

--- src/app/plane-list/plane-list.module.ts
+++ src/app/plane-list/plane-list.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatCardModule, MatButtonModule } from '@angular/material';
+import {MatCardModule, MatButtonModule, MatIconModule } from '@angular/material';
 import { PlaneListComponent } from './plane-list.component';
+import { PlaneListService } from './../services/planelist.service';
+
 
 @NgModule({
   imports: [
     CommonModule,
     MatCardModule,    
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
-  declarations: [PlaneListComponent]
+  declarations: [PlaneListComponent], 
+  providers: [PlaneListService]
 })
 export class PlaneListModule { }
