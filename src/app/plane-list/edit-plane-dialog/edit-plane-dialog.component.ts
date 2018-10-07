cb363d005e0196005fbc8458d0dcb2fb06968737
Thomas <thomasbirke@gmx.de>
2018-10-07 17:32:24 +0200

    03.005F: Add basic forms with formControls

--- src/app/plane-list/edit-plane-dialog/edit-plane-dialog.component.ts
+++ src/app/plane-list/edit-plane-dialog/edit-plane-dialog.component.ts
@@ -11,10 +11,17 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class EditPlaneDialogComponent {
 
+  numberFormControl: FormControl;
+  nameFormControl: FormControl;
+  positionFormControl: FormControl;
+
+
   constructor(
     public editPlaneDialogRef: MatDialogRef<EditPlaneDialogComponent>, public snackBar: MatSnackBar,
     @Inject(MAT_DIALOG_DATA) public plane: Plane
-  ) {  }
+  ) {
+    this.initializeFormControls();
+  }
 
 
   public onNoClick(): void {
@@ -24,5 +31,20 @@ export class EditPlaneDialogComponent {
   public saveMemberData(): void {
       this.editPlaneDialogRef.close(this.plane);
   }
+
+  public initializeFormControls(): void {
+    this.numberFormControl = new FormControl(this.plane.number, [
+      Validators.required
+    ]);
+
+    this.nameFormControl = new FormControl(this.plane.name, [
+      Validators.required
+    ]);
+
+    this.positionFormControl = new FormControl(this.plane.position, [
+      Validators.required
+    ]);
+  }
+
 
 }
