9bf290ea34013e1e7c634001af842126151387f6
Thomas <thomasbirke@gmx.de>
2018-10-07 17:36:13 +0200

    03.005F: Create update plane service

---
+++ src/app/services/plane-update.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { Plane } from './../models/plane.model';
+
+@Injectable()
+export class PlaneUpdateService {
+
+  constructor(public httpClient: HttpClient) { }
+
+  public updatePlaneData(planeData: Plane): Observable<any> {
+    const id = planeData.id;
+    const url = 'http://localhost:3000/planes' + id;
+    delete planeData['id'];
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this.httpClient.put<any>(
+      url,
+      planeData,
+      {
+        headers: headers,
+        observe: 'response'
+      }
+    );
+  }
+}
