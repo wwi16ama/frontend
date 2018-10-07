d7278c7fc4ac170054d09c95bb914247e6aaa510
Thomas <thomasbirke@gmx.de>
2018-10-07 11:05:50 +0200

    03.001F: Add test for plane list service

--- src/app/services/planelist.service.spec.ts
+++ src/app/services/planelist.service.spec.ts
@@ -1,12 +1,44 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 
 import { PlaneListService } from './planelist.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-describe('PlanelistService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+import { Plane, neededAuthorizationEnum } from './../models/plane.model';
+
+describe('MemberlistService', () => {
+
+  let injector: TestBed;
+  let service: PlaneListService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        PlaneListService
+      ]
+    });
+    injector = getTestBed();
+    service = injector.get(PlaneListService);
+    httpMock = injector.get(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const service: PlaneListService = TestBed.get(PlaneListService);
     expect(service).toBeTruthy();
+  });
+
+  it('should trigger get request', () => {
+    const testPlaneListData = [
+      new Plane(0, 'D-ERFI', 'Diamond DA-40 TDI', 'Halle 1', neededAuthorizationEnum.PPLA),
+      new Plane(1, 'D-EJEK', 'DR 400 Remorquereur', 'Halle 2', neededAuthorizationEnum.BZFII),
+    ];
+    service.getPlaneListData().subscribe(planeListData => {
+      expect(planeListData).toEqual(testPlaneListData);
+    });
+    const req = httpMock.expectOne('http://localhost:4200/assets/mock-data/plane.json');
+    expect(req.request.method).toBe('GET');
+    req.flush(testPlaneListData);
   });
 });
