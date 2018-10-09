51117796a766d487ff9815818ae1b634e0c414ed
Unknown <ykrissel@googlemail.com>
2018-10-09 10:56:56 +0200

    07.005F: G M + C

---
+++ src/app/sidebar/sidebar.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SidebarComponent]
+})
+export class SidebarModule { }
