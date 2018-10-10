7233f8733db7608d677e2d2f3927f2432f7bb1e4
Unknown <pia.rittler@gmx.de>
2018-10-10 18:27:29 +0200

    02.103F  add expensingbill in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { MemberListComponent } from './member-list/member-list.component';
 import { MemberViewComponent } from './member-view/member-view.component';
 import { PlaneListComponent } from './plane-list/plane-list.component';
+import { ExpensingBillListComponent } from './expensing-bill-list/expensing-bill-list.component';
 import { FeeListComponent } from './fee-list/fee-list.component';
 import { CreditListComponent } from './credit-list/credit-list.component';
 import { AccountComponent } from './account/account.component';
@@ -13,6 +14,8 @@ export const appRoutes: Routes = [
   { path: '', redirectTo: '/memberlist', pathMatch: 'full' },
   { path: 'memberlist', component: MemberListComponent },
   { path: 'member/:id', component: MemberViewComponent },
+  { path: 'planelist', component: PlaneListComponent },
+  { path: 'expensingbilllist', component: ExpensingBillListComponent }
   { path: 'planelist', component: PlaneListComponent },
   { path: 'feelist', component: FeeListComponent},
   { path: 'creditlist', component: CreditListComponent},
