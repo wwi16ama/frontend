727843e2c70cbcd9d758f40c0fe274eed0d294c3
unknown <marcus.henning4@gmail.com>
2018-10-19 15:13:33 +0200

    Post Requesr hinzugefügt

--- src/app/member-list/member-list.module.ts
+++ src/app/member-list/member-list.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule, MatSortModule, MatButtonModule, MatIconModule } from '@angular/material';
 import { MemberListService } from './../services/memberlist.service';
 import { AddUserFormModule } from './../add-user-form/add-user-form.module';
+import { MemberAddService } from '../services/member-add.service';
 
 @NgModule({
   imports: [
@@ -26,7 +27,8 @@ import { AddUserFormModule } from './../add-user-form/add-user-form.module';
     MemberListComponent
   ],
   providers: [
-    MemberListService
+    MemberListService,
+    MemberAddService
   ]
 })
 export class MemberListModule { }
