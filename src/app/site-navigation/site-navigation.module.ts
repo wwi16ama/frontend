b2ecac5803efbc085a2051b8c8a22187607b8872
Thomas <thomasbirke@gmx.de>
2018-10-20 15:50:41 +0200

    07.005F: Style hover button

--- src/app/site-navigation/site-navigation.module.ts
+++ src/app/site-navigation/site-navigation.module.ts
@@ -5,6 +5,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule, MatList } from '@angular/material/list';
+
 
 @NgModule({
   imports: [
@@ -12,7 +14,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatSidenavModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule
   ],
   declarations: [
     SiteNavigationComponent
