3f91b1d8873fe27c3915f954e70c9e73a4239c7b
JT <j.toengi@gmail.com>
2018-10-21 13:53:13 +0200

    03.002F-*: New Branch created to avoid rebasing conflicts

--- src/app/plane-list/plane-list.component.ts
+++ src/app/plane-list/plane-list.component.ts
@@ -195,6 +195,13 @@ export class PlaneListComponent implements OnInit {
     );
   }
 
+    dialogRef.afterClosed().subscribe(result => {
+      if (result != null) {
+        this.savePlaneData(result);
+      }
+    });
+  }
+
   public formatStringToEnum(plane: any): Plane {
     plane.neededAuthorization = neededAuthorizationEnum.getEnumString(plane.neededAuthorization);
     return plane;
