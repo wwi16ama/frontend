d9810c4a4698e68d2fb91b1504f28173d19f33d2
Unknown <simon.hannibald@googlemail.com>
2018-10-25 14:56:06 +0200

    Die Liste kriegt jetzt Transaktionen, Guthaben und Mitgliedernummer
    Noch nicht fertig: Mitgliederkontonummer und Kontonummer + Umleitung auf den richtigen Account (aktuell nur Zwischenlösung)

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Account, Type } from './../models/account.model';
+import { AccountService } from './../services/account.service';
+
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  account: Account;
+
+  constructor(
+    public accountService: AccountService, public activatedRoute: ActivatedRoute
+    ) {}
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(
+      params => {
+        // Zeile 25: funktioniert noch nicht richtig. Aktuell nur Zwischenlsöung, weil ich zu faul war
+        this.accountService.getAccountData(1).subscribe(
+          (data: Account) => {
+            this.account = data;
+            for (let i = 0; i < this.account.transactions.length; i++) {
+              this.account.transactions[i].type = Type[this.account.transactions[i].type];
+            }
+          }
+        );
+      }
+    );
   }
 
+  public formatStringToEnum(account: any): any {
+    for (let i = 0; i < account.transactions.length; i++) {
+      account.transactions[i].type = Type.getEnumString(account.transactions[i].type);
+    }
+    return account;
+  }
 }
+
+
+
+
