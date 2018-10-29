49f496cf41e29bd13a1abb8929ef518e28ac68c4
JT <j.toengi@gmail.com>
2018-10-29 21:05:08 +0100

    03.002F-: Further errors fixed

--- src/app/services/add-plane.service.spec.ts
+++ src/app/services/add-plane.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AddPlaneService } from './add-plane.service';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule} from '@angular/common/http/testing';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from '../../environments/environment';
 
 describe('AddPlaneService', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule, HttpTestingController, Observable, environment]
+    imports: [HttpClientTestingModule, Observable, environment]
   }));
 
   it('should be created', () => {
