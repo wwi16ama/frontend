89c666858cb5533622f615460f6367f98eafde00
Unknown <pia.rittler@gmx.de>
2018-10-29 12:11:11 +0100

    02.001F: Enable sorting in table

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 import { Account, Type } from './../models/account.model';
 import { AccountService } from './../services/account.service';
 import { Member } from './../models/member.model';
 import { MemberService } from './../services/member.service';
+import { MatTableDataSource, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-account',
@@ -12,29 +13,48 @@ import { MemberService } from './../services/member.service';
 })
 export class AccountComponent implements OnInit {
 
+  sort: any;
   account: Account;
   member: Member;
+  displayedColumns: string[];
+  dataSource: any;
+
+  @ViewChild(MatSort) set content(sort: ElementRef) {
+    this.sort = sort;
+    if (this.sort){
+       this.dataSource.sort = this.sort;
+  
+    }
+  }
 
   constructor(
     public accountService: AccountService, public memberService: MemberService
-    ) {}
+  ) {
+    this.displayedColumns = ['timestamp', 'amount', 'type'];
+  }
 
   ngOnInit() {
-      // Zeile 26: funktioniert noch nicht richtig. Aktuell nur Zwischenlsöung, weil ich zu faul war
-      this.accountService.getAccountData(0).subscribe(
-        (data: Account) => {
-          this.account = data;
-          for (let i = 0; i < this.account.transactions.length; i++) {
-              this.account.transactions[i].type = Type[this.account.transactions[i].type];
-          }
+    // Zeile 26: funktioniert noch nicht richtig. Aktuell nur Zwischenlsöung, weil ich zu faul war
+    this.accountService.getAccountData(0).subscribe(
+      (data: Account) => {
+
+        this.account = data;
+
+        for (let i = 0; i < this.account.transactions.length; i++) {
+          this.account.transactions[i].type = Type[this.account.transactions[i].type];
         }
-      );
-      // siehe Zeile 25
-      this.memberService.getMemberData(0).subscribe(
-        (memberdata: Member) => {
-          this.member = memberdata;
-        }
-      );
+        this.dataSource = new MatTableDataSource(this.account.transactions);
+      }
+
+    );
+    // siehe Zeile 25
+    this.memberService.getMemberData(0).subscribe(
+      (memberdata: Member) => {
+        this.member = memberdata;
+        this.dataSource.sort = this.sort;
+
+      }
+    );
   }
 
   public formatStringToEnum(account: any): any {
