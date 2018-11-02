0247d753a5812df09931358bdbb6b21674a89500
Unknown <ykrissel@googlemail.com>
2018-11-02 15:00:52 +0100

    Profilübersicht: fix Status

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef, ChangeDetectorRef } from '@an
 
 import { Account, Type } from './../models/account.model';
 import { AccountService } from './../services/account.service';
-import { Member } from './../models/member.model';
+import { Member, Status } from './../models/member.model';
 import { MemberService } from './../services/member.service';
 import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 import { MatButtonModule } from '@angular/material/button';
@@ -42,6 +42,7 @@ export class AccountComponent implements OnInit {
     this.memberService.getMemberData(0).subscribe(
       (memberdata: Member) => {
         this.member = memberdata;
+        this.member.status=Status[this.member.status];
         this.accountService.getAccountData(0).subscribe(
           (data: Account) => {
             this.account = data;
