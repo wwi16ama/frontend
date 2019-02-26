a5ae1d39cf8aa28afc1cba9eadb76bf2c1f821ce
Thomas <thomasbirke@gmx.de>
2019-02-26 15:07:05 +0100

    Refactor: Move plane-update service to plane

--- src/app/services/plane.service.ts
+++ src/app/services/plane.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { Plane } from './../models/plane.model';
 import { environment } from '../../environments/environment';
@@ -11,5 +11,20 @@ export class PlaneService {
 
   public getPlaneListData(): Observable<Plane[]>  {
     return this.httpClient.get<Plane[]>(environment.baseUrl + '/planes');
+  }
+
+  public updatePlaneData(planeData: Plane): Observable<any> {
+    const id = planeData.id;
+    const url = environment.baseUrl + '/planes/' + id;
+    delete planeData['id'];
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this.httpClient.put<any>(
+      url,
+      planeData,
+      {
+        headers: headers,
+        observe: 'response'
+      }
+    );
   }
 }
