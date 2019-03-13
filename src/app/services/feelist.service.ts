78afa487105a525f0b14d81011b97e4feddac7f2
Thomas <thomasbirke@gmx.de>
2019-03-13 14:14:29 +0100

    04.114F: Add auth header to feelist service

--- src/app/services/feelist.service.ts
+++ src/app/services/feelist.service.ts
@@ -4,15 +4,23 @@ import { Observable } from 'rxjs/internal/Observable';
 import { ListFee } from '../models/list-fee.model';
 import { environment } from './../../environments/environment';
 
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeeListService {
 
-  constructor(public httpClient: HttpClient) { }
+  constructor(public httpClient: HttpClient, public authService: AuthService) { }
 
-  public getFeeListData(): Observable<ListFee[]>  {
-    return this.httpClient.get<ListFee[]>(environment.baseUrl + '/fees');
+  public getFeeListData(): Observable<ListFee[]> {
+    const url = environment.baseUrl + '/fees';
+    const headers = this.authService.setAuthHeader();
+    return this.httpClient.get<ListFee[]>(
+      url,
+      {
+        headers: headers
+      }
+    );
   }
 }
