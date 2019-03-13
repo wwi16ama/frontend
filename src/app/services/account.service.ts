ce5a797f82629cc0c31e4aefa8468f0cfb7fc1e1
Thomas <thomasbirke@gmx.de>
2019-03-13 14:12:17 +0100

    04.114F: Add auth header to account service

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -4,14 +4,23 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Account } from './../models/account.model';
 
+import { AuthService } from './auth.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor(public httpClient: HttpClient) { }
+  constructor(public httpClient: HttpClient, public authService: AuthService) { }
 
   public getAccountData(memberBankingAccountId: number): Observable<Account> {
-    return this.httpClient.get<Account>(environment.baseUrl + '/accounts/' + memberBankingAccountId);
+    const url = environment.baseUrl + '/accounts/' + memberBankingAccountId;
+    const headers = this.authService.setAuthHeader();
+    return this.httpClient.get<Account>(
+      url,
+      {
+        headers: headers
+      }
+    );
   }
 }
