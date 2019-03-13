1c7488e94f2661018fab5981c3d53adab575a270
Thomas <thomasbirke@gmx.de>
2019-03-13 14:16:11 +0100

    04.114F: Add auth header to member service

--- src/app/services/member.service.ts
+++ src/app/services/member.service.ts
@@ -6,24 +6,39 @@ import { Member } from './../models/member.model';
 import { ListMember } from './../models/list-member.model';
 
 import { environment } from '../../environments/environment';
+import { AuthService } from './auth.service';
+
 
 @Injectable()
 export class MemberService {
 
-  constructor(public httpClient: HttpClient) { }
+  constructor(public httpClient: HttpClient, public authService: AuthService) { }
 
   public getMemberData(id): Observable<Member>  {
     const url = environment.baseUrl + '/members/' + id;
-    return this.httpClient.get<Member>(url);
+    const headers = this.authService.setAuthHeader();
+    return this.httpClient.get<Member>(
+      url,
+      {
+        headers: headers
+      }
+    );
   }
 
   public getMemberListData(): Observable<ListMember[]>  {
-    return this.httpClient.get<ListMember[]>(environment.baseUrl + '/members');
+    const url = environment.baseUrl + '/members';
+    const headers = this.authService.setAuthHeader();
+    return this.httpClient.get<ListMember[]>(
+      url,
+      {
+        headers: headers
+      }
+    );
   }
 
   public deleteMemberData (memberId: number): Observable <any> {
     const url = environment.baseUrl + '/members/' + memberId;
-    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const headers = this.authService.setAuthHeader();
     return this.httpClient.delete<any>(
       url,
       {
@@ -36,7 +51,7 @@ export class MemberService {
     const id = memberData.id;
     const url = environment.baseUrl + '/members/' + id;
     delete memberData['id'];
-    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const headers = this.authService.setAuthHeader();
     return this.httpClient.put<any>(
       url,
       memberData,
@@ -49,7 +64,7 @@ export class MemberService {
 
   public addMemberData(memberData: Member): Observable<any> {
     const url = environment.baseUrl + '/members';
-    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const headers = this.authService.setAuthHeader();
     return this.httpClient.post<any>(
       url,
       memberData,
