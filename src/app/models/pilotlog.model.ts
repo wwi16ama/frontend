a6f53ff5dc4a8f45d39928519f0faa289327e71b
Unknown <pia.rittler@gmx.de>
2019-03-14 19:15:10 +0100

    04.108F correct PilotLog model

---
+++ src/app/models/pilotlog.model.ts
@@ -0,0 +1,11 @@
+export class Pilotlog {
+    constructor(
+        public flightId: number,
+        public planeNumber: string,
+        public departureLocation: string,
+        public departureTime: string,
+        public arrivalLocation: string,
+        public arrivalTime: string,
+        public flightWithGuests: boolean
+    ) {}
+}
