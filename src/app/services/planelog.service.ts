ed7eb0e629335f9c786ead06dfee2ae16590387e
YK <ykrissel@googlemail.com>
2019-03-18 10:10:31 +0100

    04.109F: Service update

--- src/app/services/planelog.service.ts
+++ src/app/services/planelog.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { PlaneLog } from './../models/planelog.model';
+import { environment } from '../../environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class PlanelogService {
+@Injectable()
+export class PlaneLogService {
 
-  constructor() { }
+  constructor(public httpClient: HttpClient) { }
+
+  public getPlaneListData(): Observable<PlaneLog[]>  {
+    return this.httpClient.get<PlaneLog[]>(environment.baseUrl + '/planelog');
+  }
+
+  public addPlaneData(PlaneLogs: PlaneLog): Observable<any> {
+    const url = environment.baseUrl + '/planelog/';
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this.httpClient.post<PlaneLog>(
+      url,
+      PlaneLog,
+      {
+        headers: headers,
+        observe: 'response'
+      }
+    );
+  }
+
 }
