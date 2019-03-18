4bb2ec5fdd77099096a6408a117b650430c588f6
YK <ykrissel@googlemail.com>
2019-03-18 10:10:31 +0100

    04.109F: Service update

--- src/app/plane-log/plane-log.module.ts
+++ src/app/plane-log/plane-log.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
+import { forwardRef, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { PlaneLogComponent } from './plane-log.component';
 import { MatTableModule, MatButtonModule, MatSortModule, MatIconModule } from '@angular/material';
+import { PlaneLogService } from '../services/planelog.service';
 
 
 @NgModule({
@@ -10,11 +12,15 @@ import { MatTableModule, MatButtonModule, MatSortModule, MatIconModule } from '@
     CommonModule,
     MatTableModule,
     MatButtonModule,
+    HttpClientModule,
     MatSortModule,
     MatIconModule
   ],
   exports: [
-    PlaneLogComponent
+    PlaneLogComponent,
+  ],
+  providers: [
+    PlaneLogService
   ]
 })
 export class PlaneLogModule { }
