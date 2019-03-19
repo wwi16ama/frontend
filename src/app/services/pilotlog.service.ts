f085d57b122c2ff7ca07342f16b431326a40f7bb
Unknown <pia.rittler@gmx.de>
2019-03-19 22:35:31 +0100

    04.111F - Pilotlog: Add PilotLogEntry to Pilotlog service

--- src/app/services/pilotlog.service.ts
+++ src/app/services/pilotlog.service.ts
@@ -3,15 +3,29 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Pilotlog } from './../models/pilotlog.model';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PilotlogService {
 
-  constructor(public httpClient: HttpClient) { }
+  constructor(public httpClient: HttpClient, public authService: AuthService) { }
 
   public getPilotLogData(memberId): Observable<Pilotlog[]> {
     return this.httpClient.get<Pilotlog[]>(environment.baseUrl + '/pilotlog/' + memberId);
+  }
+
+  public addPilotLogEntry(pilotlog:  Pilotlog): Observable<any> {
+    const url = environment.baseUrl + '/pilotlog/' + this.authService.getMemberID() +'pilotlogentry';
+    const headers = this.authService.setAuthHeader();
+    return this.httpClient.post<Pilotlog>(
+      url,
+      pilotlog,
+      {
+        headers: headers,
+        observe: 'response'
+      }
+    );
   }
 }
