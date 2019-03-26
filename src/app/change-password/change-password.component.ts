592d81669a87c18c1bccd8bf4ce00588251384fa
Thomas <thomasbirke@gmx.de>
2019-03-26 11:44:59 +0100

    01.022F: Add basic password component

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { PasswordErrorStateMatcher } from './../error-state-matcher/password-error-state-matcher/password-error-state-matcher';
+
 
 @Component({
   selector: 'app-change-password',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  passwordFormControl: FormControl;
+  passwordFormGroup: FormGroup;
+  passwordMatcher = new PasswordErrorStateMatcher();
+
+  constructor(public formbuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.initializeFormControls();
+  }
+
+  public initializeFormControls(): void {
+    this.passwordFormControl = new FormControl('', [
+      Validators.required,
+      Validators.pattern(/([0-9].*[a-z])|([a-z].*[0-9])/),
+    ]);
+
+    this.passwordFormGroup = this.formbuilder.group({
+      password: this.passwordFormControl,
+      passwordRepeat: ['']
+    }, { validator: this.checkPasswords });
+  }
+
+  public checkPasswords(group: FormGroup) {
+    const password = group.controls.password.value;
+    const passwordRepeat = group.controls.passwordRepeat.value;
+
+    return password === passwordRepeat ? null : { passwordsNotSame: true };
   }
 
 }
--- src/app/change-password/change-password.module.ts
+++ src/app/change-password/change-password.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatInputModule, MatCheckboxModule } from '@angular/material';
 import { ChangePasswordComponent } from './change-password.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ChangePasswordComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     ChangePasswordComponent
