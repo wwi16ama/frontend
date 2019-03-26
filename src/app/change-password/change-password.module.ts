c96e8a70016134b341988a67f6ea260dfba11ad1
Thomas <thomasbirke@gmx.de>
2019-03-26 10:52:42 +0100

    01.022F: rename

---
+++ src/app/change-password/change-password.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangePasswordComponent } from './change-password.component';
+
+@NgModule({
+  declarations: [ChangePasswordComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ChangePasswordModule { }
