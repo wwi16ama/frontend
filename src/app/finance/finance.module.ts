c8d6d6630ab034c6d90daa357088f5c57e35aabd
JT <j.toengi@gmail.com>
2019-03-27 20:45:14 +0100

    02.010F: Account List added

--- src/app/finance/finance.module.ts
+++ src/app/finance/finance.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FinanceComponent } from './finance.component';
+import {MatSelectModule} from '@angular/material/select';
+import { MatIconModule, MatButtonModule, MatCheckboxModule, MatFormFieldModule,
+  MatInputModule, MatStepperModule, MatCardModule, MatTooltipModule, MatDatepickerModule,
+  MatNativeDateModule, MatDialogModule, MatTabsModule, MatTableModule } from '@angular/material';
+import { SharedDirectivesModule } from '../directives/shared-directives.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FinanceComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedDirectivesModule,
+    MatButtonModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatStepperModule,
+    MatCardModule,
+    MatTooltipModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatIconModule
   ]
 })
 export class FinanceModule { }
