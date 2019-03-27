01cb29aff583e93780a3693611b46a3d8ae7678c
JT <j.toengi@gmail.com>
2019-03-27 20:45:14 +0100

    02.010F: Account List added

--- src/app/finance/finance.component.ts
+++ src/app/finance/finance.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import { MatTableDataSource, MatDialog, MatSnackBar, MatSort } from '@angular/material';
+import { ListMember } from './../models/list-member.model';
+import { MemberService } from './../services/member.service';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Member} from './../models/member.model';
+import { DataSource } from '@angular/cdk/table';
+
 
 @Component({
   selector: 'app-finance',
@@ -6,10 +15,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./finance.component.css']
 })
 export class FinanceComponent implements OnInit {
+  sort: any;
+  dataSource: any;
+  displayedColumns: string[];
 
-  constructor() { }
 
-  ngOnInit() {
+  @ViewChild(MatSort) set content(sort: ElementRef) {
+    this.sort = sort;
+    if (this.sort) {
+      this.dataSource.sort = this.sort;
+
+    }
   }
 
-}
+  constructor(public router: Router, public addUserDialog: MatDialog, public snackBar: MatSnackBar, public activatedRoute: ActivatedRoute,
+    public memberService: MemberService) {
+    this.displayedColumns = ['id', 'firstName', 'lastName', 'memberBankingAccountId'];
+  }
+
+  ngOnInit() {
+    this.memberService.getMemberListData().subscribe(
+      (data: ListMember[]) => {
+        this.dataSource = new MatTableDataSource(data);
+        this.dataSource.sort = this.sort;
+      }
+    );
+    }
+  }
